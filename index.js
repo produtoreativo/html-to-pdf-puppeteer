29eaeb7df5c63d3cda63a96340f5dc3dc009a47b
Christiano Milfont <cmilfont@gmail.com>
2020-06-15 09:23:22 -0300

    transformando num serviço web

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
-const fs = require('fs')
-const path = require('path')
-const utils = require('util')
 const puppeteer = require('puppeteer')
 const hb = require('handlebars')
-const readFile = utils.promisify(fs.readFile)
+const axios = require('axios')
 
-async function getTemplateHtml() {
-
-    console.log("Loading template file in memory")
-    try {
-        const invoicePath = path.resolve("./invoice.html");
-        return await readFile(invoicePath, 'utf8');
-    } catch (err) {
-        return Promise.reject("Could not load html template");
-    }
+async function getTemplateHtml(url) {
+ return await axios({ method: 'get', url })
 }
 
-
-async function generatePdf() {
-
-    let data = {};
-
-    getTemplateHtml()
-        .then(async (res) => {
-            // Now we have the html code of our template in res object
-            // you can check by logging it on console
-            // console.log(res)
-
-            console.log("Compiing the template with handlebars")
+async function generatePdf(reply, data, template) {
+    getTemplateHtml(template)
+        .then(async (response) => {
+            const res = response.data;
+            console.log("Compiling the template with handlebars")
             const template = hb.compile(res, { strict: true });
-            // we have compile our code with handlebars
-            const result = template(data);
-            // We can use this to add dyamic data to our handlebas template at run time from database or API as per need. you can read the official doc to learn more https://handlebarsjs.com/
-            const html = result;
-
-            // we are using headless mode 
+            const html = template(data);
             const browser = await puppeteer.launch();
-            const page = await browser.newPage()
-
-            // We set the page content as the generated html by handlebars
-            await page.setContent(html)
-
-            // we Use pdf function to generate the pdf in the same folder as this file.
-            await page.pdf({ path: 'invoice.pdf', format: 'A4' })
-
+            const page = await browser.newPage();
+            await page.setContent(html);
+            const stream = await page.pdf({ format: 'A4' })
             await browser.close();
             console.log("PDF Generated")
-
+            reply.type('application/pdf').send(stream);
         })
         .catch(err => {
-            console.error(err)
+            console.error(err);
+            reply.send(err);
         });
 }
 
-generatePdf();
+module.exports = generatePdf
