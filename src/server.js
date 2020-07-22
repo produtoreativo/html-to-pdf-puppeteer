87fa9f28d2bc999e421866ddab90da032a7853c7
Christiano Milfont <cmilfont@gmail.com>
2020-07-22 14:01:13 -0300

    ajustando a port

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ fastify.post('/forms', (request, reply) => {
   reply.send({ success: true })
 })
 
-fastify.listen(3000, function (err, address) {
+fastify.listen(process.env.PORT || 3000, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
