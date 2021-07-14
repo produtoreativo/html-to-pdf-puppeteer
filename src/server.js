7eb82cb6cdf9d8734e653813f1ffdde4187405e5
cmilfont <cmilfont@gmail.com>
2021-07-14 15:40:07 -0300

    limpando

--- src/server.js
+++ src/server.js
@@ -3,24 +3,15 @@ const hb = require('handlebars')
 const axios = require('axios')
 const fastify = require('fastify')({ logger: true })
 
-const fs = require('fs')
-const path = require('path')
-const utils = require('util')
-const readFile = utils.promisify(fs.readFile)
-
 async function generate(reply, { data, template }) {
   const { data: rawTemplate } = await axios({ method: 'get', url: template });
-  // const res = response.data;
-  // const invoicePath = path.resolve("./certificado.html");
-  // const rawTemplate =  await readFile(invoicePath, 'utf8');
 
   const engine = hb.compile(rawTemplate, { strict: true });
   const html = engine(data);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(html);
-  const stream = await page.pdf({ 
-    // path: 'certificado2.pdf',  
+  const stream = await page.pdf({
     format: 'A4', 
     preferCSSPageSize: true,
     printBackground: true,
@@ -30,15 +21,7 @@ async function generate(reply, { data, template }) {
   reply.type('application/pdf').send(stream);
 }
 
-fastify.get('/', (request, reply) => reply.send({ hello: 'world' }))
 fastify.post('/certificado', (request, reply) => generate(reply, request.body))
-
-fastify.post('/forms', (request, reply) => {
-  console.log('Forms Office')
-  console.log('Aqui deveria vir o body')
-  console.log(request.body)
-  reply.send({ success: true })
-})
 
 fastify.listen(process.env.PORT, '0.0.0.0', function (err, address) {
   if (err) {
@@ -47,9 +30,3 @@ fastify.listen(process.env.PORT, '0.0.0.0', function (err, address) {
   }
   fastify.log.info(`server listening on ${address}`)
 })
-
-// generate(null, {
-//   data: {
-//     name: 'Christiano Milfont'
-//   }
-// })
